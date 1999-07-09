81b08553e9a0606c2b250c0099ecf0a02f78fe7c
Daniel Bonniot <bonniot@gmail.com>
1999-07-09 20:16:45 +0000

    *** empty log message ***

--- Nice/src/bossa/util/Internal.java
+++ Nice/src/bossa/util/Internal.java
@@ -11,8 +11,9 @@
 /**************************************************************************/
 
 // File    : Internal.java
+// $Author$
 // Created : Wed Jul 07 18:23:19 1999 by bonniot
-//$Modified: Fri Jul 09 21:24:26 1999 by bonniot $
+//$Modified: Fri Jul 09 22:16:29 1999 by bonniot $
 // Description : Internal errors...
 
 package bossa.util;

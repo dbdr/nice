e8a659f5f0649013ac7f0f4bb7e8d00883a82c37
Daniel Bonniot <bonniot@gmail.com>
2000-07-18 16:31:23 +0000

    Native arrays

---
+++ Nice/stdlib/nice/lang/AbstractArray.java
@@ -0,0 +1,6 @@
+public abstract class Array implements nice.lang.Sequence
+{
+  public abstract int getLength();
+  public abstract Object get(int index);
+  public abstract void set(int index, Object elem);
+}
---
+++ Nice/stdlib/nice/lang/ObjectArray.java
@@ -0,0 +1,55 @@
+public final class ObjectArray extends Array
+{
+  ObjectArray(Object[] value)
+  {
+    this.value = value;
+  }
+  
+  public static ObjectArray make(Object[] value)
+  {
+    if(value==null)
+      return null;
+    return new ObjectArray(value);
+  }
+  
+  public static Object[] newInstance(int length)
+  {
+    return new Object[length];
+  }
+  
+  public static Object[] newInstance(int length, Object sample)
+  {
+    return (Object[]) 
+      java.lang.reflect.Array.newInstance(sample.getClass(),length);
+  }
+  
+  /** Return a new copy with newSize elements */
+  public static Object[] resizeArray(Object[] from, int newSize)
+  {
+    int copyLength = from.length;
+    if(newSize<copyLength)
+      copyLength=newSize;
+    
+    Object[] res = newInstance(newSize);
+    java.lang.System.arraycopy(from,0,res,0,copyLength);
+
+    return res;
+  }
+  
+  public int getLength()
+  {
+    return value.length;
+  }
+  
+  public Object get(int index)
+  {
+    return value[index];
+  }
+  
+  public void set(int index, Object elem)
+  {
+    value[index]=elem;
+  }
+
+  public Object[] value;
+}

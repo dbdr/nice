f704c3f605ea2f4b8ab2e1d03aef71eb136c29ae
Daniel Bonniot <bonniot@gmail.com>
2000-10-02 16:30:52 +0000

    Bug in interface printing

--- Nice/src/bossa/syntax/AbstractInterface.java
+++ Nice/src/bossa/syntax/AbstractInterface.java
@@ -105,7 +105,7 @@ public class AbstractInterface extends Definition
             +name
             +Util.map("<",", ",">",parameters)
             +Util.map(" extends ",", ","",extensions)
-            +";\n");
+            +"{}\n");
   }
   
   /****************************************************************

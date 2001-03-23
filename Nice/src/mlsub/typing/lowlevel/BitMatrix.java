b3ab3aa42f10d86d900785985a040346d1f82acc
Daniel Bonniot <bonniot@gmail.com>
2001-03-23 22:46:43 +0000

    Optimization for sparse but reflexive BitMatrices

--- Nice/src/mlsub/typing/lowlevel/BitMatrix.java
+++ Nice/src/mlsub/typing/lowlevel/BitMatrix.java
@@ -3,10 +3,11 @@ package mlsub.typing.lowlevel;
 import java.util.ArrayList;
 
 /**
- * A square matrix of bits, used to represent a relation between integers.
- *
- * @version $Revision$, $Date$
- * @author Alexandre Frey
+   A square matrix of bits, used to represent a relation between integers.
+   
+   @version $Revision$, $Date$
+   @author Alexandre Frey
+   @author Daniel Bonniot (Optimization for sparse and reflexives matrices)
  **/
 final public class BitMatrix implements Cloneable {
   /**
@@ -15,6 +16,10 @@ final public class BitMatrix implements Cloneable {
    **/
   private BitVector[] rows;
   private int size;
+
+  // A BitVector instance can be saved if the only bit set is the one
+  // on the diagonal, which is usual here.
+  private boolean reflexive;
   
   /**
    * Constructs an empty matrix
@@ -75,12 +80,36 @@ final public class BitMatrix implements Cloneable {
   }
   
   /**
-   * Get ith row. May return null if it is empty or if row is beyond
-   * size()
+     To be able to return a correct row for reflexive matrices
+     without allocating a new one, we share the short ones
+     with only one bit set.
+  */
+  private static BitVector[] reflexiveRow = new BitVector[256];
+  
+  /**
+   * Get ith row. May return null if it is empty or if row is beyond size()
    **/
   BitVector getRow(int i) {
     if (i < size) {
-      return rows[i];
+      if (reflexive && rows[i] == null)
+	{
+	  if (i >= reflexiveRow.length || reflexiveRow[i] == null)
+	    {
+	      BitVector res = new BitVector(i+1);
+	      res.set(i);
+	      if (i < reflexiveRow.length)
+		reflexiveRow[i] = res;
+	      return res;
+	    }
+	  
+	  return reflexiveRow[i];
+	}
+      else
+	{
+	  if (reflexive) 
+	    rows[i].set(i);
+	  return rows[i];
+	}
     } else {
       return null;
     }
@@ -91,6 +120,9 @@ final public class BitMatrix implements Cloneable {
    * Assume i and j are valid indexes
    **/
   public boolean get(int i, int j) {
+    if (i == j && reflexive)
+      return true;
+    
     BitVector row = rows[i];
     return row != null && row.get(j);
   }
@@ -99,6 +131,9 @@ final public class BitMatrix implements Cloneable {
    * Set element at position (i, j) to true. Assume i and j are valid indexes.
    **/
   public void set(int i, int j) {
+    if (i == j && reflexive)
+      return;
+    
     BitVector row = rows[i];
     if (row == null) {
       row = new BitVector(size);
@@ -111,6 +146,15 @@ final public class BitMatrix implements Cloneable {
    * Set element at position (i, j) to false. Assume i and j are valid indexes.
    **/
   public void clear(int i, int j) {
+    if (i == j && reflexive)
+      {
+	// Outch! We have to get back to a non-reflexive representation
+	reflexive = false;
+	for (int k = 0; k<size; k++)
+	  if (k != i) 
+	    set(k, k);
+      }
+    
     BitVector row = rows[i];
     if (row != null) {
       row.clear(j);
@@ -134,9 +178,8 @@ final public class BitMatrix implements Cloneable {
         }
       }
     }
-    for (int i = 0; i < size; i++) {
-      set(i, i);
-    }
+    //for (int i = 0; i < size; i++) set(i, i);
+    reflexive = true;
   }
 
   /**
@@ -149,20 +192,13 @@ final public class BitMatrix implements Cloneable {
     for (int i = 0; i < size; i++) {
       BitVector row = rows[i];
       if (row != null) {
-        int ncols = row.size();
 	for (int j = row.getLowestSetBit();
 	     j>=0;
 	     j = row.getLowestSetBit(j+1))
 	  m.set(j, i);
-        /*
-	for (int j = 0; j < ncols; j++) {
-	  if (row.get(j)) {
-	    m.set(j, i);
-	  }
-        }
-	*/
       }
     }
+    m.reflexive = reflexive;
     return m;
   }
 
@@ -179,6 +215,7 @@ final public class BitMatrix implements Cloneable {
       }
       m.rows = v;
       m.size = size;
+      m.reflexive = reflexive;
       return m;
     } catch (CloneNotSupportedException e) {
       throw new InternalError
@@ -188,90 +225,41 @@ final public class BitMatrix implements Cloneable {
 
   
   public String toString() {
-    StringBuffer sb = new StringBuffer();
-    sb.append("{");
+    StringBuffer sb = new StringBuffer("{");
+
     boolean needSeparator = false;
-    for (int row = 0; row < size; row++) {
-      BitVector rowBitVector = rows[row];
-      if (rowBitVector != null) {
-        int ncols = rowBitVector.size();
-        for (int col = 0; col < ncols; col++) {
-          if (rowBitVector.get(col)) {
-            if (needSeparator) {
-              sb.append(", ");
-            } else {
-              needSeparator = true;
-            }
-            sb.append("(").append(row).append(",").append(col).append(")");
-          }
-        }
-      }
-    }
-    sb.append("}");
-    return sb.toString();
+
+    for (int row = 0; row < size; row++) 
+      for (int col = 0; col < size; col++)
+	if (get(row, col)) 
+	  {
+	    if (needSeparator) {
+	      sb.append(", ");
+	    } else {
+	      needSeparator = true;
+	    }
+	    sb.append("(").append(row).append(",").append(col).append(")");
+	  }
+    return sb.append("}").toString();
   }
 
   public boolean equals(Object obj) {
-    if (this == obj) {
+    if (this == obj)
       return true;
-    }
-    if (obj == null || !(obj instanceof BitMatrix)) {
-      return false;
-    } else {
-      BitMatrix that = (BitMatrix) obj;
-      BitVector[] rows1 = this.rows;
-      BitVector[] rows2 = that.rows;
-      int nrows1 = size;
-      int nrows2 = that.size;
-      int n = Math.min(nrows1, nrows2);
-      for (int i = 0; i < n; i++) {
-        BitVector row1 = rows1[i];
-        BitVector row2 = rows2[i];
-        if (row1 == null && row2 == null) continue;
-        if (row1 == null && row2.isEmpty()) continue;
-        if (row2 == null && row1.isEmpty()) continue;
-        if (row1.equals(row2)) continue;
-        return false;
-      }
-      if (nrows1 > n) {
-        for (int i = n; i < nrows1; i++) {
-          BitVector row1 = rows1[i];
-          if (row1 != null && !row1.isEmpty()) {
-            return false;
-          }
-        }
-      } else if (nrows2 > n) {
-        for (int i = n; i < nrows2; i++) {
-          BitVector row2 = rows2[i];
-          if (row2 != null && !row2.isEmpty()) {
-            return false;
-          }
-        }
-      }
-      return true;
-    } 
-  }
 
-  /**
-   * Returns true if this BitMatrix is transitively closed
-   **/
-  public boolean isClosed() {
-    for (int k = 0; k < size; k++) {
-      BitVector row_k = getRow(k);
-      if (row_k != null) {
-        for (int i = 0; i < size; i++) {
-          BitVector row_i = getRow(i);
-          if (row_i != null && row_i.get(k)) {
-            if (!row_k.includedIn(row_i)) {
-              return false;
-            }
-          }
-        }
-      }
-    }
+    if (obj == null || !(obj instanceof BitMatrix))
+      return false;
+    
+    BitMatrix that = (BitMatrix) obj;
+
+    for (int row = 0; row < size; row++) 
+      for (int col = 0; col < size; col++)
+	if (get(row, col) != that.get(row, col))
+	  return false;
+    
     return true;
-  }
-  
+  } 
+
   /**
    * Fills the array S with a topological sort of the relation on [m, size()[
    * described by this matrix. Assume that S is large enough to hold size() -
@@ -281,6 +269,11 @@ final public class BitMatrix implements Cloneable {
    * all i, j such that this.get(i, j) is true, i appears before j in S
    **/
   public void topologicalSort(int m, int[] S) {
+    // The algorithm should be modified to handle reflexive case.
+    // Easy way out: insert the reflexive points.
+    // However I'm not sure this case ever happens anyway:
+    // topologicalSort is only used in Satisfier on C which is not reflexive
+    
     int n = size;
     int[] sortStack = new int[n - m];
     int sp = -1;                // index in sortStack
@@ -340,34 +333,27 @@ final public class BitMatrix implements Cloneable {
       return null;
     }
   }
-  
+
+  public void indexMove(int src, int dest)
+  {
+    rowMove(src, dest);
+    colMove(src, dest);
+  }
 
   /**
    * In-place copy row src to row dest and clear row src. The previous results
    * of getRow(src) must not be used after this call.
    **/
-  public void rowMove(int src, int dest) {
+  private void rowMove(int src, int dest) {
     rows[dest] = getRow(src);
     rows[src] = null;
-  }
-
-  /**
-   * clear column col
-   **/
-  public void colClear(int col) {
-    for (int i = 0; i < size; i++) {
-      BitVector row = getRow(i);
-      if (row != null) {
-        row.clear(col);
-      }
-    }
   }
 
   /**
    * copy column src to column dest and clear column src.
    * Assume src != dest
    **/
-  public void colMove(int src, int dest) {
+  private void colMove(int src, int dest) {
     for (int i = 0; i < size; i++) {
       BitVector row = getRow(i);
       if (row != null) {
@@ -376,21 +362,10 @@ final public class BitMatrix implements Cloneable {
     }
   }
 
-  /**
-   * Returns the number of bits set in the square [0, n[ x [0, n[
-   **/
-  public int bitCount(int n) {
-    int cnt = 0;
-    if (size < n) {
-      n = size;
-    }
-    for (int i = 0; i < n; i++) {
-      BitVector row = getRow(i);
-      if (row != null) {
-        cnt += row.bitCount(n);
-      }
-    }
-    return cnt;
+  public void indexMerge(int src, int dest)
+  {
+    rowMerge(src, dest);
+    colMerge(src, dest);
   }
 
   /**
@@ -398,7 +373,7 @@ final public class BitMatrix implements Cloneable {
    * Assume src != dest. Previous results of getRow(src) must not be used
    * after this call.
    **/
-  public void rowMerge(int src, int dest) {
+  private void rowMerge(int src, int dest) {
     BitVector srcRow = getRow(src);
     if (srcRow != null) {
       BitVector destRow = getRow(dest);
@@ -415,7 +390,7 @@ final public class BitMatrix implements Cloneable {
    * Merge column src and column dest, put the result in column dest. Assume
    * src != dest.
    **/
-  public void colMerge(int src, int dest) {
+  private void colMerge(int src, int dest) {
     for (int i = 0; i < size; i++) {
       BitVector row = getRow(i);
       if (row != null) {

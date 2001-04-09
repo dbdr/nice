7ece15c8dafbb767c0d95136782a46dd09528f69
Daniel Bonniot <bonniot@gmail.com>
2001-04-09 16:50:57 +0000

    End of transitional state!

--- Nice/src/bossa/util/Debug.java
+++ Nice/src/bossa/util/Debug.java
@@ -88,7 +88,6 @@ public abstract class Debug
     skipLinkTests       = getBoolean("debug.skipLinkTests",false),
     passes		= getBoolean("debug.passes",false),
 
-    niceResolve         = getBoolean("debug.niceResolve", false),
     alwaysDumpStack     = getBoolean("debug.alwaysDumpStack", false),
     ignorePrelude	= getBoolean("debug.ignorePrelude", false);
   

698bc8cef72be7d307b89965761561537ca3fa35
Daniel Bonniot <bonniot@gmail.com>
2001-05-12 12:59:16 +0000

    Carries error messages, so that they can be catched up

---
+++ Nice/src/bossa/util/UserError.java
@@ -0,0 +1,21 @@
+/**************************************************************************/
+/*                                N I C E                                 */
+/*             A high-level object-oriented research language             */
+/*                        (c) Daniel Bonniot 2000                         */
+/*                                                                        */
+/*  This program is free software; you can redistribute it and/or modify  */
+/*  it under the terms of the GNU General Public License as published by  */
+/*  the Free Software Foundation; either version 2 of the License, or     */
+/*  (at your option) any later version.                                   */
+/*                                                                        */
+/**************************************************************************/
+
+package bossa.util;
+
+public class UserError extends RuntimeException
+{
+  UserError(String message)
+  {
+    super(message);
+  }
+}

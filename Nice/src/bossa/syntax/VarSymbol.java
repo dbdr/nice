c8cad6508b87936b42d7f52951dcd51c543d9e07
Daniel Bonniot <bonniot@gmail.com>
2001-06-08 22:05:53 +0000

    Solve bug with anonymous toplevel function arguments

--- Nice/src/bossa/syntax/VarSymbol.java
+++ Nice/src/bossa/syntax/VarSymbol.java
@@ -30,7 +30,11 @@ abstract class VarSymbol extends Node implements Located
   {
     super(Node.upper);
     this.name = name;
-    addSymbol(this);
+
+    // Anonymous function arguments have null names.
+    // They must not be put in the scope.
+    if (name != null)
+      addSymbol(this);
   }
 
   public boolean hasName(LocatedString i)

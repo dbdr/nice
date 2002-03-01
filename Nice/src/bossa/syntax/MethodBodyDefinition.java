053d4d841115bf13c20a71f74619158ba7bdaf56
Daniel Bonniot <bonniot@gmail.com>
2002-03-01 17:57:42 +0000

    .

--- Nice/src/bossa/syntax/MethodBodyDefinition.java
+++ Nice/src/bossa/syntax/MethodBodyDefinition.java
@@ -1,7 +1,7 @@
 /**************************************************************************/
 /*                                N I C E                                 */
 /*             A high-level object-oriented research language             */
-/*                        (c) Daniel Bonniot 2000                         */
+/*                        (c) Daniel Bonniot 2002                         */
 /*                                                                        */
 /*  This program is free software; you can redistribute it and/or modify  */
 /*  it under the terms of the GNU General Public License as published by  */

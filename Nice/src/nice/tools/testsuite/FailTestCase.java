e271ebd9ac0d0034df4be43c1b5fb5097f724000
agreif <agreif@users.sourceforge.net>
2002-06-02 02:01:50 +0000

    Testcase that checks a failure

---
+++ Nice/src/nice/tools/testsuite/FailTestCase.java
@@ -0,0 +1,26 @@
+package nice.tools.testsuite;
+
+
+
+/**
+ * TestCase class fot the case that the test should fail
+ * 
+ */
+public class FailTestCase extends TestCase {
+
+	/**
+		Performs the test for this testcase.
+		Compilation should fail, otherwise throw exception.
+	*/
+	public void performTest() throws TestSuiteException {
+		try {
+			compilePackages();
+		} catch(TestSuiteException e) {
+			return;
+		}
+		
+		throw new TestSuiteException("Compilation was expected to fail, but it succeeded.");
+	}
+
+}
+

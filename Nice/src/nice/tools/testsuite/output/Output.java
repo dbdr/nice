097356ba524376e5ea880d3e04f5bb1e51a655d3
agreif <agreif@users.sourceforge.net>
2002-06-03 16:55:25 +0000

    Testsuite output Interface

---
+++ Nice/src/nice/tools/testsuite/output/Output.java
@@ -0,0 +1,90 @@
+package nice.tools.testsuite.output;
+
+
+import java.io.*;
+import java.util.*;
+
+import nice.tools.testsuite.*;
+
+/**
+ * Appender Interface.
+ * 
+ * @author	Alex Greif <a href="mailto:alex.greif@web.de">alex.greif@web.de</a>
+ * @version	$Id$
+ */
+public interface Output {
+
+	/**
+	 * Called when the application starts up.
+	 * 
+	 */
+	public void startApplication();
+	/**
+	 * Called when the application terminates.
+	 * 
+	 */
+	public void endApplication();
+	
+	/**
+	 * Called when the testsuite starts to perform its testcases..
+	 * 
+	 * @param	testSuite	TODO
+	 */
+	public void startTestSuite(TestSuite testSuite);
+	/**
+	 * Called when the testsuite finishes to perform its testcases..
+	 * 
+	 */
+	public void endTestSuite();
+	
+	/**
+	 * Called when the testcase starts to perform.
+	 * 
+	 * @param	testCase	TODO
+	 */
+	public void startTestCase(TestCase testCase);
+	/**
+	 * Called when the testcase is performed.
+	 * 
+	 * @param	succeeded	TODO
+	 */
+	public void endTestCase(boolean succeeded);
+
+
+	/**
+	 * Logs a statement with the given prefix in angled braces.
+	 * 
+	 * @param	prefix	TODO
+	 * @param	statement	TODO
+	 */
+	public void log(String prefix, String statement);
+	/**
+	 * Logs a statement.
+	 * 
+	 * @param	statement	TODO
+	 */
+	public void log(String statement);
+	
+	
+	/**
+	 * Logs an exception.
+	 * 
+	 * @param	exception	TODO
+	 */
+	public void log(Throwable exception);
+	
+	
+	/**
+	 * Logs a statement and an exception.
+	 * 
+	 * @param	statement	TODO
+	 * @param	exception	TODO
+	 */
+	public void log(String statement, Throwable exception);
+
+
+
+}
+
+
+

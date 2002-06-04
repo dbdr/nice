7affe4a8cb4a6f8f1c2a317bdd7e2ade7cc6905f
agreif <agreif@users.sourceforge.net>
2002-06-04 21:42:05 +0000

    This class outputs the test engine messages in plain text format with indentations

---
+++ Nice/src/nice/tools/testsuite/output/TextOutput.java
@@ -0,0 +1,151 @@
+package nice.tools.testsuite.output;
+
+
+import java.io.*;
+import java.util.*;
+
+import nice.tools.testsuite.*;
+
+/**
+ * Output logs the statements as text with indentations.
+ * 
+ * @author	Alex Greif <a href="mailto:alex.greif@web.de">alex.greif@web.de</a>
+ * @version	$Id$
+ */
+public class TextOutput extends AbstractOutput {
+	/**
+	 * TODO
+	 * 
+	 */
+	private String _indentSign = "  ";
+
+	/**
+	 * TODO
+	 * 
+	 */
+	private int _indent = 0;
+
+
+
+
+
+	/**
+	 * TODO
+	 * 
+	 * @param	out	TODO
+	 */
+	public TextOutput(Writer out) {
+		super(out);
+	}
+
+
+
+	/**
+	 * TODO
+	 * 
+	 */
+	private String indent() {
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < _indent; i++)
+			buf.append(_indentSign);
+			
+		return buf.toString();
+	}
+
+
+	/**
+	 * Called when the application starts up.
+	 * 
+	 */
+	public void startApplication() {
+		logAndFlush("run test engine");
+		_indent++;
+	}
+	
+	
+	/**
+	 * Called when the application terminates.
+	 * 
+	 */
+	public void endApplication() {
+		_indent--;
+		log("test engine finished");
+		log("");
+		log("number of testcases: " + (TestNice.getTestCasesSucceeded() + TestNice.getTestCasesFailed()));
+		log("  succeeded: " + TestNice.getTestCasesSucceeded());
+		log("  failed   : " + TestNice.getTestCasesFailed());
+		logAndFlush("");
+	}
+	
+	
+	
+	/**
+	 * Called when the testsuite starts to perform its testcases..
+	 * 
+	 * @param	testSuite	TODO
+	 */
+	public void startTestSuite(TestSuite testSuite) {
+		logAndFlush("testsuite: " + testSuite.getFile());
+		_indent++;
+	}
+	
+	
+	/**
+	 * Called when the testsuite finishes to perform its testcases..
+	 * 
+	 */
+	public void endTestSuite() {
+		_indent--;
+		//log("finished");
+
+	}
+	
+	
+	
+	/**
+	 * Called when the testcase starts to perform.
+	 * 
+	 * @param	testCase	TODO
+	 */
+	public void startTestCase(TestCase testCase) {
+		mark();
+		log("testcase");
+		_indent++;
+	}
+	
+	
+	/**
+	 * Called when the testcase is performed.
+	 * 
+	 * @param	pass	TODO
+	 */
+	public void endTestCase(boolean pass) {
+		if (pass)
+			reset();
+		else
+			logAndFlush("fail");
+
+		_indent--;
+	}
+	
+	
+
+
+
+	/**
+	 * TODO
+	 * 
+	 */
+	protected String getIndent() {
+		return indent();
+	}
+
+
+
+	
+	
+
+}
+
+
+

89214972303b6b8325e4cdf72e7fa4c1a479de00
Daniel Bonniot <bonniot@gmail.com>
2003-04-02 22:25:25 +0000

    Project file for the Java Development Environment for Emacs (JDEE).

---
+++ Nice/prj.el
@@ -0,0 +1,5 @@
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-global-classpath (quote ("./classes")))
+ '(jde-compile-option-directory "./classes")
+ '(jde-compile-option-sourcepath (quote ("./src" "./stdlib"))))

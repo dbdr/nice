78eb706c3c831e759634e68c4f7273abf3ca09e2
Bryn Keller <xoltar@users.sourceforge.net>
2003-04-29 00:17:58 +0000

    Changed JDE project file to maintain indenting conventions (8 space tabs, 2 space basic indents)

--- Nice/prj.el
+++ Nice/prj.el
@@ -3,3 +3,9 @@
  '(jde-global-classpath (quote ("./classes")))
  '(jde-compile-option-directory "./classes")
  '(jde-compile-option-sourcepath (quote ("./src" "./stdlib"))))
+
+;; Standard indentation settings for Java files in Nicec.
+(setq indent-tabs-mode nil)
+(setq tab-width 8)
+(setq c-basic-offset 2)
+(c-set-offset 'substatement-open 0)

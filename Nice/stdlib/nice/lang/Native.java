e73215c270870d4bebe21857c0614c9c7ecf4d0d
Arjan Boeijink <w.a.boeijink@student.utwente.nl>
2003-09-27 11:51:52 +0000

    Changed the license in Native.java to LGPL.

--- Nice/stdlib/nice/lang/Native.java
+++ Nice/stdlib/nice/lang/Native.java
@@ -1,12 +1,12 @@
 /**************************************************************************/
 /*                                N I C E                                 */
 /*             A high-level object-oriented research language             */
-/*                        (c) Daniel Bonniot 2000                         */
+/*                        (c) Daniel Bonniot 2003                         */
 /*                                                                        */
-/*  This program is free software; you can redistribute it and/or modify  */
-/*  it under the terms of the GNU General Public License as published by  */
-/*  the Free Software Foundation; either version 2 of the License, or     */
-/*  (at your option) any later version.                                   */
+/*  This package is free software; you can redistribute it and/or modify  */
+/*  it under the terms of the GNU Lesser General Public License as        */
+/*  published by the Free Software Foundation; either version 2 of the    */
+/*  License, or (at your option) any later version.                       */
 /*                                                                        */
 /**************************************************************************/
 

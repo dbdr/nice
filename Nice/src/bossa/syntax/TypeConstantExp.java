b6724ca2478a099d005c9950a85fb749301f0efc
Daniel Bonniot <bonniot@gmail.com>
2003-12-09 17:20:16 +0000

    Unified handling of expressions that represent classes/types under TypeConstantExpression, which has a proper type and can be manipulated by user code.

--- Nice/src/bossa/syntax/TypeConstantExp.java
+++ Nice/src/bossa/syntax/TypeConstantExp.java
@@ -26,5 +26,58 @@ class TypeConstantExp extends ConstantExp
     super(PrimitiveType.typeTC, name, name.toString(), name.location());
   }
 
+  private TypeConstantExp(LocatedString name, gnu.bytecode.Type type)
+  {
+    this(name);
+    this.value = type;
+  }
+
+  /**
+     @return an Expression representing ident as a type or package literal.
+  */
+  static Expression create(LocatedString ident)
+  {
+    return create(null, ident);
+  }
+  
+  /**
+     @return an Expression representing [root].[name]
+  */
+  static Expression create(PackageExp root, LocatedString name)
+  {
+    String fullName = name.toString();
+    if (root != null)
+      fullName = root.name.append(".").append(fullName).toString();
+    
+    mlsub.typing.TypeConstructor tc = 
+      Node.getGlobalTypeScope().globalLookup(fullName, name.location());
+
+    if(tc != null)
+      {
+	gnu.bytecode.Type type = nice.tools.code.Types.javaType(tc);
+	// type might not be a class
+	// for instance if the ident was "int"
+	if (type instanceof gnu.bytecode.ClassType)
+	  {
+	    Expression res = new TypeConstantExp(name, type);
+	    res.setLocation(root == null ? name.location() : root.location());
+	    return res;
+	  }
+      }
+
+    if (root != null)
+      // name has been appended to root's name
+      return root;
+
+    root = new PackageExp(fullName);
+    root.setLocation(name.location());
+    return root;
+  }
+  
+  gnu.bytecode.ClassType staticClass()
+  {
+    return (gnu.bytecode.ClassType) value;
+  }
+  
   mlsub.typing.TypeConstructor representedType;
 }

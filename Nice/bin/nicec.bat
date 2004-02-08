29514d0cb4d1a6215583732853b4a669cafcefa3
Arjan Boeijink <w.a.boeijink@student.utwente.nl>
2004-02-08 02:36:50 +0000

    Use magnus fix to handle white spaces in directory name correctly.

--- Nice/bin/nicec.bat
+++ Nice/bin/nicec.bat
@@ -7,41 +7,49 @@ rem
 rem     NICE - the root directory where nice is installed
 rem ---------------------------------------------------------------------------
 
+
 rem -- do we have a nice environment variable?
-if not "%NICE%" == "" goto gotNice
+for %%x in (%NICE%) do goto gotNiceEnvVar
 
 rem -- try some standard places
 
-set NICE=c:\nice
-if exist %NICE%\nice.jar goto gotNice
+set NICEPATH=c:\nice
+if exist %NICEPATH%\nice.jar goto gotNice
 
-set NICE="c:\Program Files\nice"
-if exist %NICE%\nice.jar goto gotNice
+set NICEPATH="c:\Program Files\nice"
+if exist %NICEPATH%\nice.jar goto gotNice
 
-set NICE=c:\programs\nice
-if exist %NICE%\nice.jar goto gotNice
+set NICEPATH=c:\programs\nice
+if exist %NICEPATH%\nice.jar goto gotNice
 
-set NICE=d:\nice
-if exist %NICE%\nice.jar goto gotNice
+set NICEPATH=d:\nice
+if exist %NICEPATH%\nice.jar goto gotNice
 
-set NICE="d:\Program Files\nice"
-if exist %NICE%\nice.jar goto gotNice
+set NICEPATH="d:\Program Files\nice"
+if exist %NICEPATH%\nice.jar goto gotNice
 
-set NICE=d:\programs\nice
-if exist %NICE%\nice.jar goto gotNice
+set NICEPATH=d:\programs\nice
+if exist %NICEPATH%\nice.jar goto gotNice
 
+goto error
 
+:envError
+echo Could not find the file: %NICE%\nice.jar
+echo The NICE environment variable is not pointing to the right directory!
+
+:error
 echo You must set the NICE environment variable to point to the directory you've installed nice in e.g.
-echo set NICE=C:\programs\nice	
-echo (note: do not add a ';' at the end) 
+echo set NICE=C:\programs\nice	echo (note: do not add a ';' at the end)
 echo You can do it by modifying Autoexec.bat or in the system settings.
 goto end
 
+:gotNiceEnvVar
+if not exist %NICE%\nice.jar goto envError
+set NICEPATH=%NICE%
+
 :gotNice
-java -classpath %NICE%\nice.jar;%CLASSPATH% nice.tools.compiler.console.fun --runtime=%NICE%\nice.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath %NICEPATH%\nice.jar;%CLASSPATH% nice.tools.compiler.console.fun --runtime=%NICEPATH%\nice.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :end
-
-rem Local variables:
-rem coding:dos
-rem End:
+rem -- clean up a bit
+set NICEPATH=

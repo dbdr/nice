c0a278d313d69288a66f454d20c77a7a45dffd6c
Daniel Bonniot <bonniot@gmail.com>
2004-02-16 13:52:57 +0000

    Factored out the search for nice.jar on windows (arjan).

--- Nice/bin/nicec.bat
+++ Nice/bin/nicec.bat
@@ -7,45 +7,11 @@ rem
 rem     NICE - the root directory where nice is installed
 rem ---------------------------------------------------------------------------
 
+call niceclasspath
 
-rem -- do we have a nice environment variable?
-for %%x in (%NICE%) do goto gotNiceEnvVar
+for %%x in (%NICEPATH%) do goto gotNice
 
-rem -- try some standard places
-
-set NICEPATH=c:\nice
-if exist %NICEPATH%\nice.jar goto gotNice
-
-set NICEPATH="c:\Program Files\nice"
-if exist %NICEPATH%\nice.jar goto gotNice
-
-set NICEPATH=c:\programs\nice
-if exist %NICEPATH%\nice.jar goto gotNice
-
-set NICEPATH=d:\nice
-if exist %NICEPATH%\nice.jar goto gotNice
-
-set NICEPATH="d:\Program Files\nice"
-if exist %NICEPATH%\nice.jar goto gotNice
-
-set NICEPATH=d:\programs\nice
-if exist %NICEPATH%\nice.jar goto gotNice
-
-goto error
-
-:envError
-echo Could not find the file: %NICE%\nice.jar
-echo The NICE environment variable is not pointing to the right directory!
-
-:error
-echo You must set the NICE environment variable to point to the directory you've installed nice in e.g.
-echo set NICE=C:\programs\nice	echo (note: do not add a ';' at the end)
-echo You can do it by modifying Autoexec.bat or in the system settings.
 goto end
-
-:gotNiceEnvVar
-if not exist %NICE%\nice.jar goto envError
-set NICEPATH=%NICE%
 
 :gotNice
 java -classpath %NICEPATH%\nice.jar;%CLASSPATH% nice.tools.compiler.console.fun --runtime=%NICEPATH%\nice.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
@@ -53,9 +19,3 @@ java -classpath %NICEPATH%\nice.jar;%CLASSPATH% nice.tools.compiler.console.fun 
 :end
 rem -- clean up a bit
 set NICEPATH=
-
-rem Setting for Emacs so that it will treat the file as a DOS file whatever
-rem OS it is running on. Please keep this.
-rem Local variables:
-rem coding:dos
-rem End:
---
+++ Nice/bin/niceclasspath.bat
@@ -0,0 +1,44 @@
+rem -- This is a utility to determine the location of the nice.jar file
+rem -- which is needed by several Nice utilities (nicec, nicedoc, ...).
+
+rem -- do we have a nice environment variable?
+for %%x in (%NICE%) do goto gotNiceEnvVar
+
+rem -- try some standard places
+
+set NICEPATH=c:\nice
+if exist %NICEPATH%\nice.jar goto end
+
+set NICEPATH="c:\Program Files\nice"
+if exist %NICEPATH%\nice.jar goto end
+
+set NICEPATH=c:\programs\nice
+if exist %NICEPATH%\nice.jar goto end
+
+set NICEPATH=d:\nice
+if exist %NICEPATH%\nice.jar goto end
+
+set NICEPATH="d:\Program Files\nice"
+if exist %NICEPATH%\nice.jar goto end
+
+set NICEPATH=d:\programs\nice
+if exist %NICEPATH%\nice.jar goto end
+
+goto error
+
+:envError
+echo Could not find the file: %NICE%\nice.jar
+echo The NICE environment variable is not pointing to the right directory!
+
+:error
+echo You must set the NICE environment variable to point to the directory you've installed nice in e.g.
+echo set NICE=C:\programs\nice	echo (note: do not add a ';' at the end)
+echo You can do it by modifying Autoexec.bat or in the system settings.
+set NICEPATH=
+goto end
+
+:gotNiceEnvVar
+if not exist %NICE%\nice.jar goto envError
+set NICEPATH=%NICE%
+
+:end

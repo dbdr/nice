5d51be802adc5f57140ee65846de44e52ff61fdc
Daniel Bonniot <bonniot@gmail.com>
2004-10-07 16:25:06 +0000

    Simplification of CustomConstructor, now that it only concerns source constructors.

--- Nice/src/bossa/syntax/CustomConstructor.java
+++ Nice/src/bossa/syntax/CustomConstructor.java
@@ -24,44 +24,35 @@ import gnu.bytecode.MiscAttr;
 import nice.tools.code.Gen;
 
 /**
-   A user defined constructor.
+   A user defined constructor, in a source program.
  */
 
-public abstract class CustomConstructor extends UserOperator
+public class CustomConstructor extends UserOperator
 {
   public static CustomConstructor make
-    (LocatedString className, Constraint cst, FormalParameters params, 
+    (LocatedString className, Constraint cst, FormalParameters params,
      Statement body)
   {
-    return new SourceCustomConstructor(className, cst, params, body);
+    return new CustomConstructor(className, cst, params, body);
   }
 
-  CustomConstructor(LocatedString className, Constraint cst, 
-                    FormalParameters params)
+  CustomConstructor(LocatedString className, Constraint cst,
+                    FormalParameters params, Statement body)
   {
-    super(new LocatedString("<init>", className.location()), cst, 
+    super(new LocatedString("<init>", className.location()), cst,
 	  returnType(className, cst), params, Contract.noContract);
+
+    this.className = className;
+    this.body = body;
   }
 
-  void addConstructorCallSymbol()
-  {
-    mlsub.typing.Polytype type = new mlsub.typing.Polytype
-      (getType().getConstraint(), 
-       new mlsub.typing.FunType(getArgTypes(), PrimitiveType.voidType));
-    classe.addConstructorCallSymbol
-      (new MethodDeclaration.Symbol(name, type) {
-          gnu.expr.Expression compileInCallPosition()
-          {
-            return getInitializationCode(true);
-          }
-        });
-  }
+  NiceClass classe;
 
   private static Monotype returnType(LocatedString className, Constraint cst)
   {
     TypeIdent classe = new TypeIdent(className);
     classe.nullness = Monotype.sure;
-    
+
     if (cst == Constraint.True)
       return classe;
 
@@ -81,6 +72,56 @@ public abstract class CustomConstructor extends UserOperator
     return res;
   }
 
+  void addConstructorCallSymbol()
+  {
+    mlsub.typing.Polytype type = new mlsub.typing.Polytype
+      (getType().getConstraint(),
+       new mlsub.typing.FunType(getArgTypes(), PrimitiveType.voidType));
+    classe.addConstructorCallSymbol
+      (new MethodDeclaration.Symbol(name, type) {
+          gnu.expr.Expression compileInCallPosition()
+          {
+            return getInitializationCode(true);
+          }
+        });
+  }
+
+  void resolve()
+  {
+    super.resolve();
+
+    TypeConstructor tc = Node.getGlobalTypeScope().globalLookup(className);
+    TypeConstructors.addConstructor(tc, this);
+    classe = NiceClass.get(tc);
+
+    if (classe == null)
+      User.error(this,
+                 "It is impossible to add a constructor to class " + tc);
+
+    addConstructorCallSymbol();
+
+    // Save the scopes, since we need them later, but they get null'ed.
+    thisScope = scope;
+    thisTypeScope = typeScope;
+  }
+
+  private VarScope thisScope;
+  private TypeScope thisTypeScope;
+
+  void resolveBody()
+  {
+    bossa.syntax.dispatch.resolveCCThis(body, this, classe);
+    body = bossa.syntax.dispatch.analyse
+      (body, thisScope, thisTypeScope, false);
+  }
+
+  void innerTypecheck() throws TypingEx
+  {
+    super.innerTypecheck();
+
+    bossa.syntax.dispatch.typecheck(body);
+  }
+
   public void printInterface(java.io.PrintWriter s)
   {
     // Constructors are not printed, they are loaded from the bytecode.
@@ -92,114 +133,58 @@ public abstract class CustomConstructor extends UserOperator
     getCode();
   }
 
-  abstract gnu.expr.Expression getInitializationCode(boolean implicitThis);
-
-  NiceClass classe;
-
-  /****************************************************************
-   * A custom constructor defined in a source program.
-   ****************************************************************/
-
-  static class SourceCustomConstructor extends CustomConstructor
+  private Map map(TypeSymbol[] source, mlsub.typing.Monotype[] destination)
   {
-    SourceCustomConstructor(LocatedString className, Constraint cst,
-                            FormalParameters params, Statement body)
-    {
-      super(className, cst, params);
+    Map res = new HashMap(source.length);
 
-      this.className = className;
-      this.body = body;
-    }
+    for (int i = 0; i < source.length; i++)
+      res.put(source[i].toString(), Monotype.create(destination[i]));
 
-    void resolve()
-    {
-      super.resolve();
-
-      TypeConstructor tc = Node.getGlobalTypeScope().globalLookup(className);
-      TypeConstructors.addConstructor(tc, this);
-      classe = NiceClass.get(tc);
-
-      if (classe == null)
-        User.error(this, 
-                   "It is impossible to add a constructor to class " + tc);
-
-      addConstructorCallSymbol();
-
-      // Save the scopes, since we need them later, but they get null'ed.
-      thisScope = scope;
-      thisTypeScope = typeScope;
-    }
-
-    private VarScope thisScope;
-    private TypeScope thisTypeScope;
-
-    void resolveBody()
-    {
-      bossa.syntax.dispatch.resolveCCThis(body, this, classe);
-      body = bossa.syntax.dispatch.analyse
-        (body, thisScope, thisTypeScope, false);
-    }
-
-    void innerTypecheck() throws TypingEx
-    {
-      super.innerTypecheck();
-
-      bossa.syntax.dispatch.typecheck(body);
-    }
-
-    private Map map(TypeSymbol[] source, mlsub.typing.Monotype[] destination)
-    {
-      Map res = new HashMap(source.length);
-
-      for (int i = 0; i < source.length; i++)
-        res.put(source[i].toString(), Monotype.create(destination[i]));
-
-      return res;
-    }
-
-    protected gnu.expr.Expression computeCode()
-    {
-      ConstructorExp lambda = Gen.createCustomConstructor
-        ((ClassType) javaReturnType(), javaArgTypes(), getSymbols());
-
-      Gen.setMethodBody(lambda, body.generateCode());
-      classe.getClassExp().addMethod(lambda);
-      
-      // In the bytecode, we want to use the same type parameter names
-      // as the class definition, even if the source of this custom constructor
-      // renamed them locally.
-      mlsub.typing.Constraint cst = getType().getConstraint();
-      if (mlsub.typing.Constraint.hasBinders(cst))
-        parameters.substitute
-          (map(cst.binders(), classe.definition.getTypeParameters()));
-
-      lambda.addBytecodeAttribute(parameters.asBytecodeAttribute());
-      initializationCode = 
-        new QuoteExp(new InitializeProc(lambda));
-      initializationCodeImplicitThis = 
-        new QuoteExp(new InitializeProc(lambda, true));
-
-      return new QuoteExp(new InstantiateProc(lambda));
-    }
-
-    gnu.expr.Expression getConstructorInvocation(boolean omitDefaults)
-    {
-      getCode();
-      return initializationCode;
-    }
-
-    gnu.expr.Expression getInitializationCode(boolean implicitThis)
-    {
-      getCode();
-      if (implicitThis)
-        return initializationCodeImplicitThis;
-      else
-        return initializationCode;
-    }
-
-    LocatedString className;
-    Statement body;
-    gnu.expr.Expression initializationCode;
-    gnu.expr.Expression initializationCodeImplicitThis;
+    return res;
   }
+
+  protected gnu.expr.Expression computeCode()
+  {
+    ConstructorExp lambda = Gen.createCustomConstructor
+      ((ClassType) javaReturnType(), javaArgTypes(), getSymbols());
+
+    Gen.setMethodBody(lambda, body.generateCode());
+    classe.getClassExp().addMethod(lambda);
+
+    // In the bytecode, we want to use the same type parameter names
+    // as the class definition, even if the source of this custom constructor
+    // renamed them locally.
+    mlsub.typing.Constraint cst = getType().getConstraint();
+    if (mlsub.typing.Constraint.hasBinders(cst))
+      parameters.substitute
+        (map(cst.binders(), classe.definition.getTypeParameters()));
+
+    lambda.addBytecodeAttribute(parameters.asBytecodeAttribute());
+    initializationCode =
+      new QuoteExp(new InitializeProc(lambda));
+    initializationCodeImplicitThis =
+      new QuoteExp(new InitializeProc(lambda, true));
+
+    return new QuoteExp(new InstantiateProc(lambda));
+  }
+
+  gnu.expr.Expression getConstructorInvocation(boolean omitDefaults)
+  {
+    getCode();
+    return initializationCode;
+  }
+
+  gnu.expr.Expression getInitializationCode(boolean implicitThis)
+  {
+    getCode();
+    if (implicitThis)
+      return initializationCodeImplicitThis;
+    else
+      return initializationCode;
+  }
+
+  LocatedString className;
+  Statement body;
+  gnu.expr.Expression initializationCode;
+  gnu.expr.Expression initializationCodeImplicitThis;
 }
